36f92b4a2f9d85ca31ef34930fe164ceb5fc8717
Alexandra31150 <alexandrap44@gmail.com>
2020-04-29 23:42:55 +0200

    ajout d'une photo

--- hackaton/src/App.js
+++ hackaton/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import SearchTitle from './SearchTitle';
+import ImageAccueil from './ImageAccueil';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ImageAccueil />
       <SearchTitle />
       <Footer />
     </div>
