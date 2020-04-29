a416663ce73f2046bc15b4d0bab7864d6ac870de
BenjaminDexant <benjamin.dexant@gmail.com>
2020-04-29 21:52:16 +0200

    creation branch list

--- hackaton/src/App.js
+++ hackaton/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Searchbar from './Searchbar';
+import ListObjects from './ListObjects';
 
 function App() {
   return (
     <div className="App">
       <Searchbar />
+      <ListObjects />
 
     </div>
   );
---
+++ hackaton/src/ListObjects.js
@@ -0,0 +1,35 @@
+import react, { Component } from 'react';
+
+const testArr = [
+    { title: "Mug", country: "UK", image: "some url" },
+    { title: "Spoon", country: "France", image: "some url" },
+    { title: "Vineyard", country: "Italy", image: "some url" },
+  ];
+  
+
+ function ListObjects(props) {
+//     function getUrl (
+//         testArr.map( url => 
+//             )
+//     )
+
+
+
+
+
+// //     return <li>{props.value}</li>;
+// //   }
+// //   function Objects(props) {
+// //     const numbers = props.numbers;
+// //     const listItems = numbers.map((number) =>
+// //       // Correct ! La clé doit être spécifiée dans le tableau.
+// //       <ListObjects key={number.toString()}              value={number} />
+// //     );
+// //     return (
+// //       <ul>
+// //         {listItems}
+// //       </ul>
+// //     );
+  }
+  
+export default ListObjects;
