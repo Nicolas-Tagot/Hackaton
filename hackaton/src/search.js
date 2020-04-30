b9beaf7365e8a2ef756272f7d9b48cce5fe1249e
Mr-gateau <tagot.nicolas@gmail.com>
2020-04-30 09:10:34 +0200

    odification search

--- hackaton/src/search.js
+++ hackaton/src/search.js
@@ -8,7 +8,7 @@ constructor(props){
   super(props);
 this.state = {
   arrObjets : ["test","deux"],
-  pays : 'spain',
+  pays : "italie",
   periodeChecked: false,
   play: [],
   image : "",
@@ -19,7 +19,7 @@ this.state = {
 this.show = this.show.bind(this);
 this.showSui = this.showSui.bind(this);
 this.componentDidMount = this.componentDidMount.bind(this);
-this.image = this.image.bind(this);
+this.imagere = this.imagere.bind(this);
 };
 
 
@@ -29,6 +29,7 @@ this.image = this.image.bind(this);
          this.arrObjets = res.data;
         console.log(this.arrObjets)
         })
+        this.timer = setTimeout(() => this.show(), 9000)
 
   }
   componentDidUpdate(){
@@ -42,7 +43,7 @@ this.image = this.image.bind(this);
   show() {
 
     this.arrObjets = Object.values(this.arrObjets);
-    this.arrObjets = this.arrObjets[1].slice(8, 1000)
+    this.arrObjets = this.arrObjets[1].slice(8, 500)
     this.suit()
     }
 
@@ -58,6 +59,7 @@ suit(){
       ));
 
         console.log(this.state.imageArr);
+        this.timer = setTimeout(() => this.imagere(), 800)
 
 }
 showSui(){
@@ -65,7 +67,7 @@ showSui(){
 
 }
 
-image(){
+imagere(){
 
   for(let i=0; i < 101; i++){
       let solution = this.state.imageArr[i];
@@ -78,10 +80,8 @@ image(){
 
 render(){
 
-  return(
+    return(
   <div>
-  <div className="btn" onClick={this.show}> Charge </div>
-  <div className="btn" onClick={this.image}> Send </div>
 {this.state.periodeChecked
   ?<div><p>Accrochez vous !</p>
       <img className="pitchoune" src={this.state.imageArr[this.state.imageSelect]} alt={this.state.imageSelect} /></div>
