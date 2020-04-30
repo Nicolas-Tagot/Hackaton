f766349c5ed95e36a4813e9b9c42bad5e0ee4fb2
Alexandra31150 <alexandrap44@gmail.com>
2020-04-30 11:23:23 +0200

    ajout du CSS

--- hackaton/src/App.js
+++ hackaton/src/App.js
@@ -7,34 +7,32 @@ import SearchTitle from './SearchTitle';
 import ImageAccueil from './ImageAccueil';
 import Search from './search';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pays: 'france',
+    };
+    this.blue = this.blue.bind(this);
+  }
 
-class App extends React.Component{
+  blue(value) {
+    console.log(value);
+  }
 
-constructor(props){
-  super(props);
-  this.state = {
-    pays : "france",
-  };
-  this.blue = this.blue.bind(this);
-}
-
-blue(value){
-  console.log(value)
-}
-
-render(){
-  this.state.pays = this.props.paysSellect;
-  return (
-    <div className="App">
-      <Navbar />
-      <ImageAccueil />
-      <SearchTitle />
-      <Searchbar />
-      <ListObjects />
-      <Footer />
-    </div>
-  );
-}
+  render() {
+    this.state.pays = this.props.paysSellect;
+    return (
+      <div className="App">
+        <Navbar />
+        <ImageAccueil />
+        {/*<SearchTitle />*/}
+        <Searchbar />
+        {/*<ListObjects />*/}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- hackaton/src/Searchbar.js
+++ hackaton/src/Searchbar.js
@@ -1,75 +1,78 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './Searchbar.css';
-import Search from "./search";
+import Search from './search';
 //import axios from "axios";
 
 const testArr = [
-  { country: "France" },
-  { country: "Spain" },
-  { country: "Portugal" },
-  { country: "Brasil" },
-  { country: "Argentina" },
-  { country: "England" },
-  { country: "Scotland" },
-  { country: "Germany" },
-  { country: "Poland" },
-  { country: "Sweden" },
-  { country: "Ireland" },
+  { country: 'France' },
+  { country: 'Spain' },
+  { country: 'Portugal' },
+  { country: 'Brasil' },
+  { country: 'Argentina' },
+  { country: 'England' },
+  { country: 'Scotland' },
+  { country: 'Germany' },
+  { country: 'Poland' },
+  { country: 'Sweden' },
+  { country: 'Ireland' },
 ];
-
-
 
 class Searchbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       listCountry: testArr,
-      defaultInput: "Pays",
-      affiche : false,
-      value : ''
+      defaultInput: 'Pays',
+      affiche: false,
+      value: '',
     };
-this.eventer=this.eventer.bind(this);
+    this.eventer = this.eventer.bind(this);
   }
 
-
   handleClick() {
-
-
-
     const { affiche: isChecked } = this.state;
     this.setState({ affiche: !isChecked });
-    console.log("click");
-}
+    console.log('click');
+  }
 
-      eventer(event){
-        this.setState({value: event.target.value});
-        }
-
+  eventer(event) {
+    this.setState({ value: event.target.value });
+  }
 
   render() {
     return (
       <div className="searchbar-container">
-        <h3>Ou voulez vous aller ?</h3>
-        <label htmlFor="country">Pays : </label>
+        <h3>Bougez vous les fesses et saisissez un pays :</h3>
 
-        <input type="text" value={this.state.value} onChange={this.eventer} list="data-country"/>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.eventer}
+          list="data-country"
+          className="inputsearch"
+        />
 
         <datalist id="data-country">
-         {this.state.listCountry.map((item, key) => (
-           <option key={key} value={item.country} />
-         ))}
-       </datalist>
+          {this.state.listCountry.map((item, key) => (
+            <option key={key} value={item.country} />
+          ))}
+        </datalist>
 
-       <button onClick={() => this.handleClick()}>{this.state.value}</button>
+        <button
+          className="buttonsearch"
+          placeholder="Go !"
+          onClick={() => this.handleClick()}
+        >
+          {this.state.value}
+        </button>
 
-       {this.state.affiche ?  <Search paysSellect={this.state.value} /> : null }
+        {this.state.affiche ? <Search paysSellect={this.state.value} /> : null}
       </div>
     );
   }
 }
 
 export default Searchbar;
-
 
 // render() {
 //   return (
